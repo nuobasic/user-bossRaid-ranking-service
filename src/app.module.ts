5629be13d4fdee5444457e8ae3a26c78e3285469
nuobasic <nuobasic@naver.com>
2022-11-24 19:05:12 +0900

    init setting - typeorm

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          port: +configService.get('DB_PORT'),
+          host: configService.get('DB_HOST'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          migrations: [__dirname + '/src/migrations/*.ts'],
+          entities: [],
+          autoLoadEntities: true,
+          synchronize: true,
+          logging: true,
+          keepConnectionAlive: true,
+        };
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
